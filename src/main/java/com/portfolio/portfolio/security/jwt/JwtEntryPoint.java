185fe7d79994a8d398df648f74caf1fea174b31e
DarioJavierCamacho <Darioj1990@gmail.com>
2022-10-12 18:30:53 -0300

    Agregado entrypoint provider y filter

--- src/main/java/com/portfolio/portfolio/security/jwt/JwtEntryPoint.java
+++ src/main/java/com/portfolio/portfolio/security/jwt/JwtEntryPoint.java
@@ -1,13 +1,24 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.portfolio.portfolio.security.jwt;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
 
 /**
  *
- * @author dario
+ * @author dario comprueba si hay un token valido, sino devuelve respuesta 401
+ * no autorizado
  */
-public class JwtEntryPoint {
-    
+@Component
+public class JwtEntryPoint implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "No autorizado");
+    }
+
 }
