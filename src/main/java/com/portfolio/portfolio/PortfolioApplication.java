efb3e3306b45952d39db4864762b3b0cdcb0e2f7
DarioJavierCamacho <Darioj1990@gmail.com>
2022-11-01 03:03:08 -0300

    agregado system properties para solucionar error de deploy

--- src/main/java/com/portfolio/portfolio/PortfolioApplication.java
+++ src/main/java/com/portfolio/portfolio/PortfolioApplication.java
@@ -19,7 +19,7 @@ public class PortfolioApplication {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("http://localhost:4200").allowedMethods("*").allowedHeaders("*");
+				registry.addMapping("/**").allowedOrigins("https://portfoliodariocamacho.web.app").allowedMethods("*").allowedHeaders("*");
 			}
 		};
 	}
