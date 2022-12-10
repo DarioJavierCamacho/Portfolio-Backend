175f3097e0070918b027bab70ba5a75ed2d1d5bb
DarioJavierCamacho <Darioj1990@gmail.com>
2022-12-10 20:21:57 -0300

    Error de cors

--- src/main/java/com/portfolio/portfolio/controller/Controller.java
+++ src/main/java/com/portfolio/portfolio/controller/Controller.java
@@ -10,6 +10,7 @@ import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,6 +26,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author dario
  */
 @RestController
+@CrossOrigin(origins = "https://portfoliodariocamacho.web.app")
 public class Controller {
 
     @Autowired
